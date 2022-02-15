54a17e1498dbc63a275e5c88566e84822db05549
TaraCarette <terra.the.great@gmail.com>
2022-02-15 23:24:22 +0100

    Complete q8 code, fully working

--- assgn1_GA/q8_code.py
+++ assgn1_GA/q8_code.py
@@ -2,12 +2,13 @@ from gplearn.genetic import SymbolicRegressor
 from gplearn.functions import make_function
 from gplearn.fitness import make_fitness
 import csv
+import matplotlib.pyplot as plt
 import numpy as np
 
 FILENAME = "q8_data.txt"
 
 
-# extract the data from the file and put it into a matrix
+# extract the data from the file 
 def readFile(fileN):
     with open(fileN, "r") as csvFile:
         csvReaderOutput = csv.reader(csvFile)
@@ -19,36 +20,54 @@ def readFile(fileN):
 
     return data
 
+# define the exp for the function list to use as it is not default
 def expFunc(x):
-    return x ** 2
+    return np.exp(x)
 
-# def fitFunc(y, yPred, sampleWeight):
-#     total = 0
-
-#     for i in range(0, len(y)):
-#         total += abs(y[i] - yPred[i])
-
-#     return total
-# fit = make_function(function=fitFunc, name='fit', arity=3)#, greater_is_better=False)
-
-exp = make_function(function=expFunc, name='exp', arity=1)
-function_set = ['add', 'sub', 'mul', 'div', 'log', 'sin', 'cos', exp]
+# define fitness function
+def fitFunc(y, yPred, sampleWeight):
+    return - np.sum(np.abs(y - yPred))
 
 
+# make own fitness function since not a default
+fit = make_fitness(function=fitFunc, greater_is_better=True)#name='fit', arity=3)#, greater_is_better=False)
+
+expV = make_function(function=expFunc, name='exp', arity=1)
+
+# the function set to be used to solve the problem
+function_set = ['add', 'sub', 'mul', 'div', 'log', 'sin', 'cos', expV]
+
+# get the data and format it so it can be processed
 data = readFile(FILENAME)
 data = np.array(data)
+
+X = np.transpose([data[0]])
+y = data[1]
+
 
 # no mutation, only crossover is goal
 # seems like exp is not allowed
 gp = SymbolicRegressor(generations=50, population_size=1000,
                          function_set=function_set,
-                         metric="mean absolute error",
+                         metric=fit,
                          p_crossover=0.7, p_subtree_mutation=0,
                          p_hoist_mutation=0, p_point_mutation=0,
                          max_samples=0.9, verbose=1,
                          random_state=0)
 
-X = np.transpose([data[0]])
-y = data[1]
+# fit the data using the regressor
+equation = gp.fit(X, y)
+print(equation)
 
-gp.fit(X, y)
+# plot the progress
+plt.plot(list(range(0, 50)), gp.run_details_["best_fitness"])
+plt.xlabel('Iteration')
+plt.ylabel('Best Fitness')
+plt.title('Best Fitness over Time')
+plt.show()
+
+plt.plot(list(range(0, 50)), gp.run_details_["best_length"])
+plt.xlabel('Iteration')
+plt.ylabel('Best Length')
+plt.title('Best Length over Time')
+plt.show()
